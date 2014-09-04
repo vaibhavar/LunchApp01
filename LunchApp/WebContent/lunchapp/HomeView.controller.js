186efc9c14dba1b11d9c07332128735f24485f60
harshit.jain <hjain.tech@gmail.com>
2014-09-04 15:19:03 +0530

    ADDED A LINE TO HOMEVIEW.CONTROLLER.JS

--- LunchApp/WebContent/lunchapp/HomeView.controller.js
+++ LunchApp/WebContent/lunchapp/HomeView.controller.js
@@ -1,5 +1,5 @@
 sap.ui.controller("lunchapp.HomeView", {
-
+//TESTING CHANGES IN THE CODE
 /**
 * Called when a controller is instantiated and its View controls (if available) are already created.
 * Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
